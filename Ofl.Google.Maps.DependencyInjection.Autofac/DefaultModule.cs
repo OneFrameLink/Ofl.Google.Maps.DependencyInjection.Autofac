f93eb67a113276f58a8517fb9170d01d93a2afa6
casperOne <casperOne@caspershouse.com>
2017-03-03 10:59:41 -0500

    Initial build.

---
+++ Ofl.Google.Maps.DependencyInjection.Autofac/DefaultModule.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Autofac;
+
+namespace Ofl.Google.Maps.DependencyInjection.Autofac
+{
+    public class DefaultModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            // Validate parameters.
+            if (builder == null) throw new ArgumentNullException(nameof(builder));
+
+            // Bind.
+            builder.RegisterType<IMapsClient>().As<IMapsClient>();
+        }
+    }
+}
---
+++ Ofl.Google.Maps.DependencyInjection.Autofac/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Ofl.Google.Maps.DependencyInjection.Autofac")]
+[assembly: AssemblyDescription("Ofl.Google.Maps.Contract implementation registrations in Autofac.")]
+[assembly: AssemblyProduct("Ofl.Google.Maps.DependencyInjection.Autofac")]

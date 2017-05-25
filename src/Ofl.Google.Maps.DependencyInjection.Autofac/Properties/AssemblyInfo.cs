85e5be7efe631ac1003ca1555c9db7496dd66753
casperOne <casperOne@caspershouse.com>
2017-05-25 08:37:47 -0400

    Removed dependency on Ofl.Core.

--- src/Ofl.Google.Maps.DependencyInjection.Autofac/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Ofl.Google.Maps.DependencyInjection.Autofac")]
-[assembly: AssemblyDescription("Ofl.Google.Maps.Contract implementation registrations in Autofac.")]
-[assembly: AssemblyProduct("Ofl.Google.Maps.DependencyInjection.Autofac")]
